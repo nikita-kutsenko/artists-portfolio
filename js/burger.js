4f2aca9e54a955558e3cd7e778cb0277e66524b3
Nikita Kutsenko <nickktsnk@gmail.com>
2020-06-28 13:50:44 +0300

    version2.1 adaptive

--- js/burger.js
+++ js/burger.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('.nav-burger').click(function () {
         $('.wrapper').toggleClass('active-nav');
     });
+    $('.nav-gallery-link').click(function () {
+        $('.nav-gallery-dropdown').toggleClass('nav-gallery-active');
+    });
 });
